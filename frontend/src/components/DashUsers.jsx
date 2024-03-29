0e9fb8844630d9f9ae7af88cf1388dcce1dac276
anubhav <anubhavt632@gmail.com>
2024-03-29 23:17:55 +0530

    add delete functionality to the admin user

--- frontend/src/components/DashUsers.jsx
+++ frontend/src/components/DashUsers.jsx
@@ -60,7 +60,22 @@ export default function DashUsers() {
   };
 
  //handle delete later
- const handleDeleteUser = async() => {};
+ const handleDeleteUser = async() => {
+    try {
+        const res = await fetch(`/backend/user/delete/${userIdToDelete}`,{
+            method:'DELETE',
+        });
+        const data = await res.json();
+        if (res.ok) {
+            setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
+            setShowModal(false);
+        } else {
+            console.log(data.message);
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+ };
 
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 
