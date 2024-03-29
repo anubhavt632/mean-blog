61151605df8be15c9d9ea6febd09fc4befaa2255
anubhav <anubhavt632@gmail.com>
2024-03-29 11:46:44 +0530

    add create a post api route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -30,6 +31,7 @@ app.use(cookieParser());
 
 app.use('/backend/user', userRoutes);
 app.use('/backend/auth', authRoutes);
+app.use('/backend/post', postRoutes);
 
 app.use((err, req, res, next)=>{
     const statusCode= err.statusCode || 500;
