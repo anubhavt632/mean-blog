8b6730632d4a117ffa36aa44eb22dadbc8faa19f
anubhav <anubhavt632@gmail.com>
2024-04-01 02:39:35 +0530

    compleate search page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
+import { useEffect, useState } from 'react';
 import {Button, Navbar, TextInput, Dropdown, Avatar} from 'flowbite-react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate} from 'react-router-dom';
 import {AiOutlineSearch} from 'react-icons/ai';
 import {FaMoon,FaSun} from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
@@ -9,9 +10,24 @@ import { signoutSuccess } from '../redux/user/userSlice';
 
 export default function Header() {
     const path = useLocation().pathname;
+    const location = useLocation();
+    const navigate = useNavigate();
     const dispatch = useDispatch();
     const { currentUser} = useSelector(state => state.user)
     const { theme } = useSelector((state) => state.theme);
+    const [searchTerm, setSearchTerm] = useState('');
+    
+
+    useEffect(() => {
+      const urlParams = new URLSearchParams(location.search);
+      const searchTermFromUrl = urlParams.get('searchTerm');
+      if (searchTermFromUrl) {
+        setSearchTerm(searchTermFromUrl);
+      }
+    }, [location.search]);
+
+
+
     const handleSignout = async () => {
       try {
           const res = await fetch ('/backend/user/signout', {
@@ -28,7 +44,14 @@ export default function Header() {
       } catch (error) {
           console.log(error.message);
       }
-  }    
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
 
   return (
     <Navbar className='border-b-2'>
@@ -38,12 +61,14 @@ export default function Header() {
             via-purple-500 to-pink-500 rounded-lg text-white'>Lagom</span>
             Blog
         </Link>
-        <form>
+        <form onSubmit={handleSubmit}>
             <TextInput
             type='text'
             placeholder='Search...'
             rightIcon={AiOutlineSearch}
             className='hidden lg:inline'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
             />
         </form>
 
