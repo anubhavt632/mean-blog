4dcff2dcc0efbf7565e5b5642b9b32eff3d9fcbc
anubhavt632 <92858183+anubhavt632@users.noreply.github.com>
2024-04-04 23:00:47 +0530

    Update SignIn.jsx

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -23,7 +23,7 @@ export default function SignIn() {
       dispatch(signInStart());
       const res = await fetch('https://mern-blog-rig7.onrender.com/backend/auth/signin', {
         method:'POST',
-        headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
+        headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'},
         body:JSON.stringify(formData),
       });
       const data = await res.json();
